221274fd7c502e25135973f5d4ba828ef99cbfdf
Matthew Vaughan <matthew@mjvaughan.plus.com>
2018-06-04 22:23:26 +0100

    MAC Modified

--- GitTest2/Program.cs
+++ GitTest2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +19,10 @@ namespace GitTest2
 
             // Another MAC modification
             // Another Laptop modification
+
+            /*
+            MAC Modified
+            */
         }
     }
 }
